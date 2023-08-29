8c6471201f3eee69743583773478e5cbbba6b578
Shanavas Shaji <shanavascruise@gmail.com>
2023-08-29 12:33:15 +0530

    Added basic ui elements

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Shanavas Shaji on 29/08/23.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
