589ecce080862e77b11ca6f0ed3b463b435209ab
Shanavas Shaji <shanavascruise@gmail.com>
2023-08-30 11:37:20 +0530

    Added buttons and properties

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -8,7 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var playerCard = "card7"
+    var cpuCard = "card13"
+    
+    var playerScore = 0;
+    var cpuScore = 0;
+    
     var body: some View {
+        
         ZStack {
             Image("background-plain")
                 .resizable()
@@ -19,13 +27,20 @@ struct ContentView: View {
                 Spacer()
                 HStack{
                     Spacer()
-                    Image("card2")
+                    Image(playerCard)
                     Spacer()
-                    Image("card3")
+                    Image(cpuCard)
                     Spacer()
                 }
                 Spacer()
-                Image("button")
+                
+                Button {
+                    dealCard()
+                } label: {
+                    Image("button")
+                }.foregroundColor(.white)
+
+                
                 Spacer()
                 HStack{
                     Spacer()
@@ -33,7 +48,7 @@ struct ContentView: View {
                         Text("Player")
                             .font(.headline)
                             .padding(.bottom, 10.0)
-                        Text("0")
+                        Text(String(playerScore))
                             .font(.largeTitle)
                     }.foregroundColor(.white)
                     Spacer()
@@ -41,7 +56,7 @@ struct ContentView: View {
                         Text("CPU")
                             .font(.headline)
                             .padding(.bottom, 10.0)
-                        Text("0")
+                        Text(String(cpuScore))
                             .font(.largeTitle)
                     }.foregroundColor(.white)
                     Spacer()
@@ -49,6 +64,9 @@ struct ContentView: View {
                 Spacer()
             }
         }
+    }
+    func dealCard() {
+        print("Deal cards")
     }
 }
 
